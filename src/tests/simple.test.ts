a2923eb22b29da6ac9709375032e7cf532dca994
yudyagilev <yu.dyagilev@gmail.com>
2024-02-11 18:14:12 +0200

    fix

--- src/tests/simple.test.ts
+++ src/tests/simple.test.ts
@@ -1,110 +1,35 @@
 import { Selector } from 'testcafe';
+import { searchInGoogle } from './roles';
 
 fixture `Getting Started`
-    .page `http://google.com.ua`;
 
 test('Test 1 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
-
-    await t
-        .click(Selector('.header-right-nav a').nth(0))
-        .click(Selector('a').withText('Install TestCafe'))
-        .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
-        .expect(Selector('code').withText('node --version').visible).ok()
-        .expect(Selector('code').withText('npm --version').visible).ok()
-        .expect(Selector('code').withText('npm install -g npm@latest').visible).ok()
-        .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
-        .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
-        .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
+    await testSteps(t);
 });
 
 test('Test 2 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
-
-    await t
-        .click(Selector('.header-right-nav a').nth(0))
-        .click(Selector('a').withText('Install TestCafe'))
-        .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
-        .expect(Selector('code').withText('node --version').visible).ok()
-        .expect(Selector('code').withText('npm --version').visible).ok()
-        .expect(Selector('code').withText('npm install -g npm@latest').visible).ok()
-        .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
-        .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
-        .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
+    await testSteps(t);
 });
 
 test('Test 3 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
-
-    await t
-        .click(Selector('.header-right-nav a').nth(0))
-        .click(Selector('a').withText('Install TestCafe'))
-        .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
-        .expect(Selector('code').withText('node --version').visible).ok()
-        .expect(Selector('code').withText('npm --version').visible).ok()
-        .expect(Selector('code').withText('npm install -g npm@latest').visible).ok()
-        .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
-        .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
-        .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
+    await testSteps(t);
 });
 
 test('Test 4 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
-
-    await t
-        .click(Selector('.header-right-nav a').nth(0))
-        .click(Selector('a').withText('Install TestCafe'))
-        .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
-        .expect(Selector('code').withText('node --version').visible).ok()
-        .expect(Selector('code').withText('npm --version').visible).ok()
-        .expect(Selector('code').withText('npm install -g npm@latest').visible).ok()
-        .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
-        .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
-        .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
+    await testSteps(t);
 });
 
 test('Test 5 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
-
-    await t
-        .click(Selector('.header-right-nav a').nth(0))
-        .click(Selector('a').withText('Install TestCafe'))
-        .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
-        .expect(Selector('code').withText('node --version').visible).ok()
-        .expect(Selector('code').withText('npm --version').visible).ok()
-        .expect(Selector('code').withText('npm install -g npm@latest').visible).ok()
-        .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
-        .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
-        .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
+    await testSteps(t);
 });
 
 test('Test 6 - Checking installation commands in TestCafe portal', async t => {
-    await t
-        .typeText('[name=q]', 'TestCafe io').pressKey('enter')
-        .click(Selector('a').withText('testcafe.io').nth(0))
-        .expect(Selector('.description').innerText).eql('TestCafe is a user-friendly end-to-end testing solution. Free and open-source test runner. Powerful desktop app.');
+    await testSteps(t);
+});
 
+async function testSteps(t: TestController) {
     await t
+        .useRole(searchInGoogle)
         .click(Selector('.header-right-nav a').nth(0))
         .click(Selector('a').withText('Install TestCafe'))
         .expect(Selector('code').withText('npm i -g testcafe').visible).ok()
@@ -114,5 +39,6 @@ test('Test 6 - Checking installation commands in TestCafe portal', async t => {
         .expect(Selector('code').withText('curl -qL https://www.npmjs.com/install.sh | sh').visible).ok()
         .expect(Selector('code').withText('npm install --save @ffmpeg-installer/ffmpeg').visible).ok()
         .expect(Selector('code').withText('yarn init').visible).ok()
-        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok()
-});
+        .expect(Selector('code').withText('yarn add -D testcafe').visible).ok();
+}
+
